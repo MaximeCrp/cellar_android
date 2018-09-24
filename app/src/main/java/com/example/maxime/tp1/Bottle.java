67fb5434b9c1b01f42e2e888270eedfd1dbb30c2
Maxime Crampon <maxime.crampon@gmail.com>
2018-09-24 11:41:44 +0200

    add Bottle class

---
+++ app/src/main/java/com/example/maxime/tp1/Bottle.java
@@ -0,0 +1,16 @@
+package com.example.maxime.tp1;
+
+public class Bottle {
+
+    private String name;
+    private Integer price;
+
+    Bottle(String nm, Integer pr) {
+        name = nm;
+        price = pr;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
